780e71c6fb64cc17f32c70d5f62ea9ffd0d30cff
AnnaToro <annunadur@gmail.com>
2023-06-30 14:57:27 -0300

    Agregando espacio para seleccionar la edad en el formulario

--- formulario-preferencias/src/App.js
+++ formulario-preferencias/src/App.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 function Formulario() {
     const [sexo, setSexo] =
     useState('');
+    const [edad, setEdad] =
+    useState('');
 }
 
 export default App;
