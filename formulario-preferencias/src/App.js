8f6e73e2d218369f8b8e6e695c161690360d4714
AnnaToro <annunadur@gmail.com>
2023-06-30 16:30:46 -0300

    Terminando el formulario de preferencias de contenido

--- formulario-preferencias/src/App.js
+++ formulario-preferencias/src/App.js
@@ -9,21 +9,61 @@ function Formulario() {
     useState('');
 
     const handleSexoChange = (e) => {
-      setSexo(e.target.value);
+        setSexo(e.target.value);
     };
     const handleEdadChange = (e) => {
-      setEdad(e.target.value);
+        setEdad(e.target.value);
     };
     const handlePreferenciasChange = (e) => {
-      setPreferencias(e.target.value);
+        setPreferencias(e.target.value);
     };
     const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log('Sexo:', sexo);
-      console.log('Edad:', edad);
-      console.log('Preferencias:', preferencias);
+        e.preventDefault();
+        console.log('Sexo:', sexo);
+        console.log('Edad:', edad);
+        console.log('Preferencias:', preferencias);
     };
+    return ( <
+        form onSubmit = { handleSubmit } >
+        <
+        label >
+        Sexo:
+        <
+        select value = { sexo }
+        onChange = { handleSexoChange } >
+        <
+        option value = "" > Seleccionar < /option> <
+        option value = "Masculino" > Masculino < /option> <
+        option value = "Femenino" > Femenino < /option> <
+        /select> <
+        /label> <
+        br / >
+        <
+        label >
+        Edad:
+        <
+        input type = "text"
+        value = { edad }
+        onChange = { handleEdadChange }
+        /> <
+        /label> <
+        br / >
+        <
+        label >
+        Preferencias:
+        <
+        textarea value = { preferencias }
+        onChange = { handlePreferenciasChange }
+        /> <
+        /label> <
+        br / >
+        <
+        button type = "submit" > Enviar < /button> <
+        /form>
+    );
+}
 
+export default Formulario;
 }
 
 export default App;
