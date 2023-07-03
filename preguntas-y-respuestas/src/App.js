f9bb14b56c79248ff4f6abb392d221bfbb9414b6
ori <oriihermanowyc@gmail.com>
2023-07-02 22:04:51 -0300

    Se agrego segunda pregunta a preguntas y respuestas

--- preguntas-y-respuestas/src/App.js
+++ preguntas-y-respuestas/src/App.js
@@ -2,7 +2,7 @@ import random
 
 preguntas = {
     "Que significa ESI?": "Entendemos la ESI una acción orientada a la educación de los niños y adolescentes, que teniendo en cuenta la pluralidad de dimensiones constitutivas de la persona, su integridad física como espiritual, los prepara para manejar adecuadamente sus afectos, a la vez que les informa científicamente sobre cuestiones relacionadas a su sexualidad.",
-    "Cual ?": "Amazonas", 
+    "Como te das cuentas que estas en una relacion violenta?": "Violencia física y verbal, golpes, malas palabras, gritos, insultos no deben ser tolerados. Si tu pareja te critica constantemente o usa tus partes vulnerables para lastimarte, eso es violencia. Si evitas mezclar a tu pareja con amigos o familiares porque temes sentirse humillada o humillado, si tu pareja ha tratado de alejarte de tus seres queridos en un intento de controlar tu vida, este aislamiento es un indicio de violencia. En una relación cada persona debe responsabilizarse de sus acciones y emociones.", 
     "Cual?": "Rusia",
     "Cual?": "Pacífico",
     "Cua?": "Torre Eiffel"
