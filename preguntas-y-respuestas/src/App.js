031d50177e6e196314d7dc4691bead76ece7a5a1
ori <oriihermanowyc@gmail.com>
2023-07-02 22:01:28 -0300

    Se agrego primera pregunta a preguntas y respuestas

--- preguntas-y-respuestas/src/App.js
+++ preguntas-y-respuestas/src/App.js
@@ -1,7 +1,7 @@
 import random
 
 preguntas = {
-    "Cual ?": "Madrid",
+    "Que significa ESI?": "Entendemos la ESI una acción orientada a la educación de los niños y adolescentes, que teniendo en cuenta la pluralidad de dimensiones constitutivas de la persona, su integridad física como espiritual, los prepara para manejar adecuadamente sus afectos, a la vez que les informa científicamente sobre cuestiones relacionadas a su sexualidad.",
     "Cual ?": "Amazonas", 
     "Cual?": "Rusia",
     "Cual?": "Pacífico",
