ebbacea09bcd37d1471602f62f6a5c4adf2edc7d
ori <oriihermanowyc@gmail.com>
2023-07-04 12:52:24 -0300

    Se hicieron cambios en preguntas y respuestas

--- preguntas-y-respuestas/src/App.js
+++ preguntas-y-respuestas/src/App.js
@@ -24,63 +24,7 @@ function App() {
 
 export default App;
 
-{
-    "preguntas": [
-      {
-        "tema": "Género y diversidad",
-        "pregunta": "¿Cuál es el concepto de identidad de género?"
-      }
-    
-}
-{
-    "tema": "Género y diversidad",
-    "pregunta": "¿Qué es la orientación sexual?"
-  },
-},
-{
-  "tema": "Educación sexual",
-  "pregunta": "¿Qué es la educación sexual integral?"
-},
-{
-    "tema": "Educación sexual",
-    "pregunta": "¿Cuáles son los métodos anticonceptivos más comunes?"
-  },
-  {
-    "tema": "Prevención de violencia",
-    "pregunta": "¿Cuáles son los tipos de violencia de género?"
-  },
-  {
-    "tema": "Prevención de violencia",
-    "pregunta": "¿Qué es el consentimiento en una relación?"
-  },
-  {
-    "tema": "Cuidados sexuales",
-    "pregunta": ""
-  },
-  {
-    "tema": "Cuidados sexuales",
-    "pregunta": ""
-  },
-  {
-    "tema": "Derechos humanos",
-    "pregunta": "¿Cuáles son los derechos humanos fundamentales?"
-  },
-  {
-    "tema": "Derechos humanos",
-    "pregunta": "¿Qué es la discriminación y cómo se combate?"
-  },
-  {
-    "tema": "Salud emocional",
-    "pregunta": "¿Qué es la inteligencia emocional?"
-  },
-  {
-    "tema": "Salud emocional",
-    "pregunta": "¿Cómo se puede gestionar el estrés?"
-  }                                                                                                                                                                                  {
-    "tema": "Cuidados de la salud y el cuerpo",
-    "pregunta": "" }
-]
-}
+
 
 
 
