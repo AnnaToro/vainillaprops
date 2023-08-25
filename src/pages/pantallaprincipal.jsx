08c5a6c40c10d2e34e86773233d6d4c7afeac086
ori <oriihermanowyc@gmail.com>
2023-08-25 19:08:57 -0300

    se empezo a agregar codigo a pantallaprincipal.jsx

--- src/pages/pantallaprincipal.jsx
+++ src/pages/pantallaprincipal.jsx
@@ -1,2 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>ESI educacion sexual integral</Text>
+      <Text style={styles.subtitle}>pantalla principal</Text>
+    </View>
