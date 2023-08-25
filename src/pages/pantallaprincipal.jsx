71bfc8321300fddbf84c542b475f47fad5809df8
ori <oriihermanowyc@gmail.com>
2023-08-25 19:12:45 -0300

    se empezo a agregar codigo a pantallaprincipal.jsx

--- src/pages/pantallaprincipal.jsx
+++ src/pages/pantallaprincipal.jsx
@@ -6,3 +6,23 @@ const HomeScreen = () => {
       <Text style={styles.title}>ESI educacion sexual integral</Text>
       <Text style={styles.subtitle}>pantalla principal</Text>
     </View>
+     );
+    };
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 10,
+      },
+      subtitle: {
+        fontSize: 18,
+        color: 'gray',
+      },
+    });
+
+    export default HomeScreen;
