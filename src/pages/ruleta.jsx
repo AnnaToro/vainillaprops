8c53a4c1bf7e34d4ffeedd2ec0ea5603a4c43912
AnnaToro <annunadur@gmail.com>
2023-09-05 21:32:38 -0300

    Agregando los temas a la ruleta de ESI

---
+++ src/pages/ruleta.jsx
@@ -0,0 +1,9 @@
+const temasESI = [
+    'Género y diversidad',
+    'Educación sexual',
+    'Cuidados sexuales',
+    'Cuidado de la salud y el cuerpo',
+    'Prevención de violencia',
+    'Derechos humanos',
+    'Salud emocional',
+];
