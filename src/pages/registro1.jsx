7dd998b4c352c19752868b0140a0b814fd0da937
AnnaToro <annunadur@gmail.com>
2023-09-06 00:17:28 -0300

    Dando espacio para completar los datos

--- src/pages/registro1.jsx
+++ src/pages/registro1.jsx
@@ -1,1 +1,12 @@
 import React, { Component } from 'react';
+class Registro extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        nombre: '',
+        apellido: '',
+        email: '',
+        contraseña: '',
+        confirmarContraseña: '',
+      };
+    }
