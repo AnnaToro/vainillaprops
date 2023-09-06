33c7d78131f3302e83369e60703fa8b487c406ec
AnnaToro <annunadur@gmail.com>
2023-09-05 21:51:30 -0300

    Continuando con el formulario de preferencias

--- src/pages/formulariodepreferencias.jsx
+++ src/pages/formulariodepreferencias.jsx
@@ -5,3 +5,18 @@ function Formulario() {
     useState('');
     const [preferencias, setPreferencias] =
     useState('');
+    const handleSexoChange = (e) => {
+        setSexo(e.target.value);
+    };
+    const handleEdadChange = (e) => {
+        setEdad(e.target.value);
+    };
+    const handlePreferenciasChange = (e) => {
+        setPreferencias(e.target.value);
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Sexo:', sexo);
+        console.log('Edad:', edad);
+        console.log('Preferencias:', preferencias);
+    };
