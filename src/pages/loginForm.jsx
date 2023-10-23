cdbbc818d04699eb473846bc3d825ec80fe88d08
AnnaToro <annunadur@gmail.com>
2023-10-23 10:40:12 -0300

    Cambiando nombre de archivos

---
+++ src/pages/loginForm.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes agregar la lógica para autenticar al usuario
+    console.log(`Username: ${username} - Password: ${password}`);
+    // También puedes redirigir al usuario a otra página después del inicio de sesión
+  }
+
+  return (
+    <div className="login-form">
+      
+      <img src="/logo.png" alt="Logo" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Nombre de usuario o correo"
+          value={username}
+          onChange={handleUsernameChange}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Contraseña"
+          value={password}
+          onChange={handlePasswordChange}
+          required
+        />
+        <button type="submit">Iniciar</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
