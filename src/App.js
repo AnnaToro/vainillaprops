a731211c0106705f738a41b8b28ea3c74cef4c7b
ori <oriihermanowyc@gmail.com>
2023-10-24 21:31:24 -0300

    Se realizan pruebas de codigo, sin todavia poder ver las pantallas.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import login from './components'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h2>Iniciar sesión</h2>
+    <form id="loginForm">
+        <label for="username">Nombre de usuario:</label>
+        <input type="text" id="username" name="username" required>
+        <br>
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" name="password" required>
+        <br>
+        <input type="submit" value="Iniciar sesión">
+    </form>
+    <p id="loginMessage"></p>
+    <script src="login.js"></script>   
       </header>
     </div>
   );
