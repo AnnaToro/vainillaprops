479cb119d7250e1346eef64145e496e0752cd26a
AnnaToro <annunadur@gmail.com>
2023-11-09 23:05:08 -0300

    Definiendo los estilos

--- src/pages/editarPerfil.jsx
+++ src/pages/editarPerfil.jsx
@@ -2,3 +2,30 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
+const useStyles = makeStyles((theme) => ({
+    container: {
+       display: 'flex',
+       flexDirection: 'column',
+       alignItems: 'center',
+       backgroundColor: '#F3F4F6',
+       padding: '16px',
+       borderRadius: '4px',
+    },
+    image: {
+       width: '80px',
+       height: '80px',
+       marginBottom: '16px',
+       borderRadius: '50%',
+       objectFit: 'cover',
+    },
+    text: {
+       marginBottom: '8px',
+    },
+    button: {
+       marginTop: '8px',
+    },
+    imageUpload: {
+       display: 'none',
+    },
+   }));
+   
